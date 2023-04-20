772db8c8ff0a8b19361ebe0d8d74d32e92660816
Axel <axelsp050@gmail.com>
2023-04-20 12:21:25 -0400

    added 4 attributes, 1 random constructor, getters & setters, 2 helper methods, and a toString

--- AdventureGame/adventuregame/src/main/java/edu/guilford/Player.java
+++ AdventureGame/adventuregame/src/main/java/edu/guilford/Player.java
@@ -12,15 +12,26 @@ public class Player {
 
     // attributes
     private int playerHP;
+    private int monsterHP;
+    private String position;
     private String weapon;
 
-    // constructors
+    // player constructor
     public Player(int HP, String weapon) {
         this.playerHP = HP;
         this.weapon = weapon;
     }
 
-    // methods
+    // random hp constructor
+    public int randomHP() {
+        int min = 1;
+        int max = 100;
+        int range = max - min + 1;
+        int randomHP = (int) (Math.random() * range) + min;
+        return randomHP;
+    }
+
+    // getters and setters
     public int getPlayerHP() {
         return playerHP;
     }
@@ -37,4 +48,35 @@ public class Player {
         this.weapon = weapon;
     }
 
+    public int getMonsterHP() {
+        return monsterHP;
+    }
+
+    public void setMonsterHP(int monsterHP) {
+        this.monsterHP = monsterHP;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Player{" + "playerHP=" + playerHP + ", monsterHP=" + monsterHP + ", position=" + position + ", weapon="
+                + weapon + '}';
+    }
+
+    // helper methods
+    public void attack() {
+        System.out.println("You attack the monster with your " + weapon);
+    }
+
+    public void checkHealth() {
+        System.out.println("Your health right now is " + playerHP);
+    }
 }
