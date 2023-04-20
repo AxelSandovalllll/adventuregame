45b89a96b06aeafb7d7429452f2641fdee3dd273
Axel <axelsp050@gmail.com>
2023-04-20 13:11:11 -0400

    added 4 attributes, 2 constructors, getters & setters, and 2 helper methods

--- src/main/java/edu/guilford/Player.java
+++ src/main/java/edu/guilford/Player.java
@@ -12,15 +12,26 @@ public class Player {
 
     // attributes
     private int playerHP;
+    private int monsterHP;
     private String weapon;
+    private String position;
 
-    // constructors
+    // player constructor
     public Player(int HP, String weapon) {
         this.playerHP = HP;
         this.weapon = weapon;
     }
 
-    // methods
+    // random playerhp constructor
+    public int randomHP() {
+        int min = 1;
+        int max = 100;
+        int range = max - min + 1;
+        int randomHP = (int) (Math.random() * range) + min;
+        return randomHP;
+    }
+
+    // getters and setters
     public int getPlayerHP() {
         return playerHP;
     }
@@ -29,12 +40,44 @@ public class Player {
         this.playerHP = playerHP;
     }
 
+    public int getMonsterHP() {
+        return monsterHP;
+    }
+
+    public void setMonsterHP(int monsterHP) {
+        this.monsterHP = monsterHP;
+    }
+
     public String getWeapon() {
         return weapon;
     }
 
     public void setWeapon(String weapon) {
         this.weapon = weapon;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    // tostring method
+    @Override
+    public String toString() {
+        return "Player{" + "playerHP=" + playerHP + ", monsterHP=" + monsterHP + ", weapon=" + weapon + ", position="
+                + position + '}';
+    }
+
+    // helper methods
+    public void attack() {
+        System.out.println("You attack the monster with your " + weapon);
+    }
+
+    public void healthCheck() {
+        System.out.println("Your current health is " + playerHP);
     }
 
 }
